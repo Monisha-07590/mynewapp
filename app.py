29b04e70698ef39c2d4a76e58c5a9ec4e7039f0a
Monisha-07590 <monishagowda733@gmail.com>
2025-03-26 15:01:55 +0530

    starting app 1

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/home')
+def home1():
+    return "<h1>Monisha</h1>"
+
+@app.route('/bub')
+def monisha():
+    return render_template('index.html')
+
+@app.route('/chatbot')
+def chatbot():
+    return render_template('chatbot.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
